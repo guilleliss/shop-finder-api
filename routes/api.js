f82c27ef51634779ebcf22a68b88df4de806e106
guilleliss <lissguillermo@gmail.com>
2015-07-30 08:48:24 +0000

    add geohash to the api

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,15 @@ var mongoose = require('mongoose');
 var Shop = mongoose.model('Shop');
 var Review = mongoose.model('Review');
 
+var geohash = require('ngeohash');
+
 /* GET home page. */
 router.get('/', function(req, res) {
 	res.json({ message: 'Welcome to the api' });
 });
 
+/* Get a list of all shops */
 router.get('/shops', function(req, res) {
-
 	Shop.find({
 	}, function (err, data) {
 		if (err) return console.error(err);
@@ -19,6 +21,7 @@ router.get('/shops', function(req, res) {
 	});	
 });
 
+/* Save a new shop, and its reviews */
 router.post('/shops', function(req, res) {
 	var reviews = req.body.reviews;
 	var reviews_count = 0;
@@ -26,6 +29,8 @@ router.post('/shops', function(req, res) {
 	if(reviews) {
 		reviews_count = reviews.length;
 	}
+
+	geohash = encodeCoords(req.body.geolocation);
 
 	var newShop = new Shop({
 		name: req.body.name,
@@ -35,7 +40,10 @@ router.post('/shops', function(req, res) {
 		opening_hours: req.body.opening_hours,
 		website: req.body.website,
 		photos: req.body.photos,
-		geolocation: req.body.geolocation,
+		geolocation: {
+			geohash: geohash,
+			location: req.body.geolocation	
+		},
 		hidden: false,
 		source: req.body.source,
 		source_id: req.body.source_id,
@@ -48,6 +56,7 @@ router.post('/shops', function(req, res) {
 	newShop.save(function(err, shop) {
 		if (err) res.send(shop);
 
+		/* If the shop brings reviews, we save each */
 		if(reviews) {		
 			for (var i = 0; i < reviews_count; i++) {
 				var newReview = new Review({
@@ -67,6 +76,7 @@ router.post('/shops', function(req, res) {
 	});	
 });
 
+/* Get a shops detail by id, but not its reviews  */
 router.get('/shops/:shop_id', function(req, res) {
 	Shop.findById(
 		req.params.shop_id, 
@@ -84,6 +94,7 @@ router.get('/reviews', function(req, res) {
 	});	
 });
 
+/* Get a list of a shop's reviews */
 router.get('/shops/:shop_id/reviews', function(req, res) {
 	Review.find({
 		shop_id: req.params.shop_id 
@@ -93,6 +104,9 @@ router.get('/shops/:shop_id/reviews', function(req, res) {
 	});	
 });
 
+/* Check if a shop exists given its source id
+ * (ie, Google Places ID)
+ */
 router.get('/shops/exists/:source_id', function(req, res) {
 	Shop.find({
 		source_id: req.params.source_id
@@ -103,12 +117,14 @@ router.get('/shops/exists/:source_id', function(req, res) {
 	});
 });
 
+/* Remove a stored shop given its id */
 router.delete('/shops/:shop_id', function(req, res) {
 	Shop.remove({
 		_id: req.params.shop_id
 	}, function (err, data) {
 		if (err) res.send(err);
 
+		/* We remove all its reviews as well */
 		Review.remove({
 			shop_id: req.params.shop_id
 		}, function (err, data) {
@@ -120,14 +136,22 @@ router.delete('/shops/:shop_id', function(req, res) {
 	});
 });
 
+/* Empty the DB, removes all shops and reviews */
 router.get('/removeall', function(req, res) {
 	Shop.remove({}, function(err) { 
-		console.log('collection removed') 
-	});
-	Review.remove({}, function(err) { 
-		console.log('collection removed') 
+		Review.remove({}, function(err) { 
+			res.json({ message: 'All data removed' });
+		});
 	});
 });
+
+/**
+ * Encodes coordantes. Parameter should be a json
+ * with lan and lon parameters.
+ */
+function encodeCoords(location) {
+	return geohash.encode(location['lat'], location['lng']);
+}
 
 /* TODO: Remove all from here */
 var GooglePlaces = require('google-places');
