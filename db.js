825c056e870cdd81cb8c46cc75982ac187961752
guilleliss <lissguillermo@gmail.com>
2015-11-09 16:44:06 +0100

    add openshift vars to database file

--- db.js
+++ db.js
@@ -18,6 +18,15 @@ if(process.env.VCAP_SERVICES){
 	mongourl = generate_mongo_url(mongo);
 }
 
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+	mongourl = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+	process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+	process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+	process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+	process.env.OPENSHIFT_APP_NAME;
+}
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
